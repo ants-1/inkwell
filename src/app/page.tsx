0c0236cb52670a1edd978e32aff1c1af1611fa7e
Anthony <anthonysalisbury98@outlook.com>
2025-08-31 09:18:00 +0100

    Create sidebar UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Sidebar from "@/components/Sidebar";
 import { Button, HStack } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <HStack>
+    <HStack backgroundColor="gray.800">
+      <Sidebar />
       <Button>Click Me</Button>
     </HStack>
   );
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,63 @@
+import { Box, VStack, Menu, Portal } from "@chakra-ui/react";
+import { Bell, NotepadText, UsersRound, Wrench, UserRound } from "lucide-react";
+import Link from "next/link";
+
+const sidebarItems = [
+  { href: "/notes", icon: NotepadText },
+  { href: "/teams", icon: UsersRound },
+  { href: "/notifications", icon: Bell },
+  { href: "/settings", icon: Wrench },
+];
+
+export default function Sidebar() {
+  const isAuth = true;
+
+  return (
+    <Box
+      w={{ base: "16", md: "20" }}
+      height="dvh"
+      backgroundColor="gray.700"
+      pt="5"
+    >
+      <VStack display="flex" alignItems="center" gap="2" height="full">
+        {sidebarItems.map(({ href, icon: Icon }, i) => (
+          <Link key={i} href={href}>
+            <Box boxSize={{ base: 10, md: 11 }} p="2" _hover={{ backgroundColor: "gray.900"}} rounded="xl">
+              <Icon color="white" height="full" width="full" />
+            </Box>
+          </Link>
+        ))}
+        <Menu.Root>
+          <Menu.Trigger asChild>
+            <Box boxSize={{ base: 10, md: 11 }} p="2" _hover={{ backgroundColor: "gray.900"}} rounded="xl">
+              <UserRound color="white" height="full" width="full" cursor="pointer" />
+            </Box>
+          </Menu.Trigger>
+          <Portal>
+            <Menu.Positioner>
+              <Menu.Content>
+                {isAuth ? (
+                  <Box>
+                    <Link href="/profile">
+                      <Menu.Item value="profile">Profile</Menu.Item>
+                    </Link>
+                    <Menu.Item value="logout">Logout</Menu.Item>
+                  </Box>
+                ) : (
+                  <Box>
+                    <Link href="/login">
+                      <Menu.Item value="login">Login</Menu.Item>
+                    </Link>
+                    <Link href="/sign-up">
+                      <Menu.Item value="sign-up">Sign-Up</Menu.Item>
+                    </Link>
+                  </Box>
+                )}
+              </Menu.Content>
+            </Menu.Positioner>
+          </Portal>
+        </Menu.Root>
+      </VStack>
+    </Box>
+  );
+}
