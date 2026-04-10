382307d175d3e91e03047398fdd1b013fed77083
Anthony <anthonysalisbury98@outlook.com>
2026-04-10 22:44:50 +0100

    feat: add middleware (morgan & cors)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,21 @@
 import express, { Application } from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 const app: Application = express();
 
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "http:localhost:5001",
+    credentials: true,
+  }),
+);
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World - Authentication Server' });
+});
 
 export default app;
